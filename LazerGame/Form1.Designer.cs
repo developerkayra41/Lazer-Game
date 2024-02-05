770740f05c99eade11c447a213956fb42b080073
Kayra <Kayra@DESKTOP-UD1PCRV>
2024-02-05 12:50:02 +0300

    Add project files.

---
+++ LazerGame/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace LazerGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.shuttlePlayer = new System.Windows.Forms.PictureBox();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.labelScore = new System.Windows.Forms.Label();
+            this.labelHak = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.shuttlePlayer)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.panel1.Location = new System.Drawing.Point(0, 880);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1860, 5);
+            this.panel1.TabIndex = 1;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 20;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // shuttlePlayer
+            // 
+            this.shuttlePlayer.Image = global::LazerGame.Properties.Resources.playerShuttlePNG;
+            this.shuttlePlayer.Location = new System.Drawing.Point(0, 890);
+            this.shuttlePlayer.Name = "shuttlePlayer";
+            this.shuttlePlayer.Size = new System.Drawing.Size(60, 80);
+            this.shuttlePlayer.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.shuttlePlayer.TabIndex = 0;
+            this.shuttlePlayer.TabStop = false;
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Interval = 1000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // labelScore
+            // 
+            this.labelScore.AutoSize = true;
+            this.labelScore.Font = new System.Drawing.Font("HoloLens MDL2 Assets", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelScore.ForeColor = System.Drawing.Color.Red;
+            this.labelScore.Location = new System.Drawing.Point(12, 9);
+            this.labelScore.Name = "labelScore";
+            this.labelScore.Size = new System.Drawing.Size(131, 40);
+            this.labelScore.TabIndex = 2;
+            this.labelScore.Text = "Score: 0";
+            // 
+            // labelHak
+            // 
+            this.labelHak.AutoSize = true;
+            this.labelHak.Font = new System.Drawing.Font("HoloLens MDL2 Assets", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelHak.ForeColor = System.Drawing.Color.Red;
+            this.labelHak.Location = new System.Drawing.Point(49, 49);
+            this.labelHak.Name = "labelHak";
+            this.labelHak.Size = new System.Drawing.Size(94, 40);
+            this.labelHak.TabIndex = 3;
+            this.labelHak.Text = "HP: 3";
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(1842, 973);
+            this.Controls.Add(this.labelHak);
+            this.Controls.Add(this.labelScore);
+            this.Controls.Add(this.shuttlePlayer);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "1860; 1020";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.shuttlePlayer)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox shuttlePlayer;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Label labelScore;
+        private System.Windows.Forms.Label labelHak;
+    }
+}
+
---
+++ LazerGame/Form1.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LazerGame
+{
+    public partial class Form1 : Form
+    {
+        Random rnd = new Random();
+        List<PictureBox> lazers = new List<PictureBox>();
+        List<PictureBox> enemies = new List<PictureBox>();
+        string yon;
+        int _hiz = 20;
+        int _lazerHiz = 10;
+        int _enemySpeed = 6;
+        int score = 0;
+        int hak = 3;
+        public Form1()
+        {
+            InitializeComponent();
+            this.DoubleBuffered = true;
+            this.BackgroundImage = Properties.Resources.backSpace;
+            this.Icon = Properties.Resources.Space_Fighter;
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (yon == "sag")
+            {
+                if (shuttlePlayer.Right <= 1830)
+                    shuttlePlayer.Left += _hiz;
+            }
+            if (yon == "sol")
+            {
+                if (shuttlePlayer.Left >= 10)
+                    shuttlePlayer.Left -= _hiz;
+            }
+
+            for (int i = 0; i < lazers.Count; i++)
+            {
+                lazers[i].Top -= _lazerHiz;
+                if (lazers[i].Top == -100) { lazers[i].Dispose(); lazers.RemoveAt(i); }
+            }
+
+            for (int i = 0; i < enemies.Count; i++) enemies[i].Top += _enemySpeed;
+
+            for (int i = 0; i < lazers.Count; i++)
+            {
+                for (int j = 0; j < enemies.Count; j++)
+                {
+                    if (lazers[i].Bounds.IntersectsWith(enemies[j].Bounds))
+                    {
+                        lazers[i].Dispose();
+                        enemies[j].Dispose();
+                        lazers.RemoveAt(i);
+                        enemies.RemoveAt(j);
+
+                        score++;
+                        labelScore.Text = string.Format("Score: {0}", score);
+                        if (score == 100)
+                        {
+                            timer1.Enabled = false;
+                            timer2.Enabled = false;
+                            MessageBox.Show("YOU WIN !");
+                            Application.Exit();
+                        }
+                        break;
+                    }
+                }
+            }
+
+            for (int i = 0; i < enemies.Count; i++)
+            {
+                if (enemies[i].Top >= 1020)
+                {
+                    hak--;
+                    labelHak.Text = string.Format("HP: {0}", hak);
+                    enemies[i].Dispose();
+                    enemies.RemoveAt(i);
+
+                    if (hak == 0)
+                    {
+                        timer1.Enabled = false;
+                        timer2.Enabled = false;
+                        MessageBox.Show("YOU LOSE !");
+                        Application.Exit();
+                    }
+                }
+            }
+
+        }
+
+        private void Fire()
+        {
+            PictureBox lazer = new PictureBox();
+            lazer.Image = Properties.Resources.lazerPNG;
+            lazer.SizeMode = PictureBoxSizeMode.StretchImage;
+            lazer.Size = new Size(12, 100);
+            lazer.Location = new Point(shuttlePlayer.Location.X + 25, shuttlePlayer.Location.Y - 80);
+
+
+            lazers.Add(lazer);
+            this.Controls.Add(lazer);
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.D) yon = "sag";
+            if (e.KeyCode == Keys.A) yon = "sol";
+            if (e.KeyCode == Keys.Space) Fire();
+        }
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.D) yon = string.Empty;
+            if (e.KeyCode == Keys.A) yon = string.Empty;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CreateEnemy()
+        {
+            PictureBox enemy = new PictureBox();
+            enemy.Size = new Size(60, 68);
+            enemy.Image = Properties.Resources.enemyShuttlePNG;
+            enemy.SizeMode = PictureBoxSizeMode.StretchImage;
+            enemy.Location = new Point(rnd.Next(1800), -80);
+            enemies.Add(enemy);
+            this.Controls.Add(enemy);
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            CreateEnemy();
+            timer2.Interval = rnd.Next(700, 1600);
+        }
+    }
+}
